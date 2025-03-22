189ef143a0329ed292a548a0dc2fdce7640ecd48
Ousssama Driouech <thedriwsh@gmail.com>
2025-03-22 05:14:21 +0000

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 
 import React from 'react'
-import { BrowserRouter , Route , Routes } from 'react-router-dom'
-import Navbar from './components/navbar'
-import About from './components/about'
-import Contact from './components/contact'
-import Greetingpage from './components/greetingpage'
+
 
 function App() {
   return (
    <React.Fragment>
-       <BrowserRouter>
-       <Navbar/>
-        <Routes>
-            <Route  path='About' element={<About/>} />
-            <Route path='contact' element={<Contact/>} />
-            <Route path='greetingpage' element={<Greetingpage/>} />
-        </Routes>
-    </BrowserRouter>
+     
    </React.Fragment>
   )
 }
--- src/components/greetingpage.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function Greetingpage() {
-  return (
-    <h1>hello all My name is Oussama</h1>
-  )
-}
-
-export default Greetingpage
